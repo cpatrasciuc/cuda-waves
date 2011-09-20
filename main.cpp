05d05d5aee8a411313a5e900df35ba9b420d7a47
Cristian Patrasciuc <cristi@linux-fs91.site>
2011-09-20 18:12:44 +0300

    Waves on CPU

--- main.cpp
+++ main.cpp
@@ -1,34 +1,68 @@
+#include <time.h>
+#include <sys/time.h>
+#include <stdio.h>
+
 #include <GL/gl.h>
 #include <GL/glu.h>
 #include <GL/freeglut.h>
 
-#define WIDTH 400
-#define HEIGHT 400
+#include "wave.h"
+
+#define WIDTH 700
+#define HEIGHT 700
+
+#define WAVES_COUNT 4
+
+Wave waves[WAVES_COUNT] =
+{
+    {320, 100, 3, 1, 0, 0},
+    {320, 60, 5, 5, 2, 0},
+    {160, 40, 2, 8, 1, 0},
+    {80, 40, 1, -4, 1, 0}
+};
 
 unsigned char bitmap[WIDTH][HEIGHT];
 
-void computeBitmap()
+void computeBitmap(float t)
 {
     for (int i = 0; i < WIDTH; i++)
     {
-        if (i % 20 < 10)
+        for (int j = 0; j < HEIGHT; j++)
         {
-            for (int j = 0; j < HEIGHT; j++)
+            float sum = 0.0;
+            for (int k = 0; k < WAVES_COUNT; k++)
             {
-                bitmap[i][j] = 255;
+                sum += computeWave(&waves[k], i, j, t);
             }
+            bitmap[i][j] = sum / WAVES_COUNT  + (255 / 2);
         }
     }
 }
 
 void display(void)
 {
-    computeBitmap();
+    static int t = 0;
+    timeval start, end_frame, end_compute;
+
+    gettimeofday(&start, NULL);
+
+    t += 10;
+    computeBitmap((float) t);
+
+    gettimeofday(&end_compute, NULL);
+
     glClear(GL_COLOR_BUFFER_BIT);
     glColor3f (1.0, 1.0, 1.0);
     glRasterPos2i(-1, -1);
     glDrawPixels(WIDTH, HEIGHT, GL_LUMINANCE, GL_UNSIGNED_BYTE, bitmap);
-    glFlush();
+    glutSwapBuffers();
+    glutPostRedisplay();
+
+    gettimeofday(&end_frame, NULL);
+
+    printf("Frame time: %d ms, Computation time: %d ms\n",
+           (end_frame.tv_sec - start.tv_sec) * 1000 + (end_frame.tv_usec - start.tv_usec),
+           (end_compute.tv_sec - start.tv_sec) * 1000 + (end_compute.tv_usec - start.tv_usec));
 }
 
 int main(int argc, char **argv)
@@ -40,7 +74,7 @@ int main(int argc, char **argv)
     glutCreateWindow("CUDA Demo");
     glPixelStorei (GL_UNPACK_ALIGNMENT, 1);
     glClearColor (0.0, 1.0, 0.0, 0.0);
-    glutDisplayFunc(display);
-    
+    glutIdleFunc(display);
+
     glutMainLoop();
 }
---
+++ wave.h
@@ -0,0 +1,24 @@
+#ifndef WAVE_H
+#define WAVE_H
+
+#include <cmath>
+
+struct Wave
+{
+    float wavelength;
+    float amplitude;
+    float speed;
+    float dx;
+    float dy;
+    float phase;
+};
+
+inline float computeWave(const Wave *w, float x, float y, float t)
+{
+    // W(x,y,t) = A x sin(D * (x,y) x w + t x phi)
+    float tmp = std::sin((w->dx * x + w->dy * y) / w->wavelength +
+                         t * w->speed * 0.01 + w->phase) + 1;
+    return w->amplitude * tmp * tmp / 2.0;
+}
+
+#endif // WAVE_H
