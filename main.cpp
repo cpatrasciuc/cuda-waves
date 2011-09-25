888f8ecff73c7ee224309620784a7a449bbaccb0
Cristian Patrasciuc <cristi@linux-fs91.site>
2011-09-25 15:07:21 +0300

    First CUDA kernel

--- main.cpp
+++ main.cpp
@@ -11,8 +11,8 @@
 
 #include "wave.h"
 
-#define WIDTH 640
-#define HEIGHT 640
+#define WIDTH 1600
+#define HEIGHT 1600
 
 #define WAVES_COUNT 4
 
@@ -27,39 +27,51 @@ Wave waves[WAVES_COUNT] =
 unsigned char bitmap[WIDTH][HEIGHT];
 
 unsigned char *dev_bitmap;
-int *dev_t;
+int *dev_time;
 Wave *dev_waves;
 
 dim3 grids(WIDTH/16, HEIGHT/16);
 dim3 threads(16, 16);
 
-__global__ void computeBitmap(int t, unsigned char *bitmap)
+__global__ void computeBitmap(Wave *dev_waves, int *t, unsigned char *bitmap)
 {
-    int i = threadIdx.x + blockIdx.x * blockDim.x;
-    int j = threadIdx.y + blockIdx.y * blockDim.y;
+    int x = threadIdx.x + blockIdx.x * blockDim.x;
+    int y = threadIdx.y + blockIdx.y * blockDim.y;
 
     float sum = 0.0;
     for (int k = 0; k < WAVES_COUNT; k++)
     {
-        Wave *w = dev_waves[k];
-        float tmp = std::sin((w->dx * x + w->dy * y) / w->wavelength +
-                             t * w->speed * 0.01 + w->phase) + 1;
-        sum += w->amplitude * tmp * tmp / 2.0;
+        Wave w = dev_waves[k];
+        float tmp = sin((w.dx * x + w.dy * y) / w.wavelength +
+                             *t * w.speed * 0.001 + w.phase) + 1;
+       /* float tmp = sin((w->dx * x + w->dy * y) / w->wavelength +
+                                     *t * w->speed * 0.001 + w->phase) + 1;*/
+        sum += w.amplitude * tmp * tmp / 2.0;
     }
-    bitmap[i][j] = sum / WAVES_COUNT  + (255 / 2);
+    bitmap[x*WIDTH + y] = sum / WAVES_COUNT  + (255 / 2);
 }
 
 void display(void)
 {
     static int t = 0;
     timeval start, end_frame, end_compute;
+    cudaEvent_t start_event, stop_event;
+    float cudaTime;
 
     gettimeofday(&start, NULL);
+    cudaEventCreate(&start_event);
+    cudaEventCreate(&stop_event);
+    cudaEventRecord(start_event, 0);
 
     t += 10;
-    cudaMemcpy(dev_t, &t, sizeof(int), cudaMemcpyHostToDevice);
+    cudaMemcpy(dev_time, &t, sizeof(int), cudaMemcpyHostToDevice);
 
-    computeBitmap<<<grids, threads>>>(t, dev_bitmap);
+    computeBitmap<<<grids, threads>>>(dev_waves, dev_time, dev_bitmap);
+
+    cudaMemcpy(bitmap, dev_bitmap, WIDTH * HEIGHT * sizeof(unsigned char), cudaMemcpyDeviceToHost);
+
+    cudaEventRecord(stop_event, 0);
+    cudaEventSynchronize(stop_event);
 
     gettimeofday(&end_compute, NULL);
 
@@ -72,15 +84,20 @@ void display(void)
 
     gettimeofday(&end_frame, NULL);
 
-    printf("Frame time: %d ms, Computation time: %d ms\n",
+    cudaEventElapsedTime(&cudaTime, start_event, stop_event);
+
+    printf("Frame time: %d ms, Computation time: %.1f ms\n",
            (end_frame.tv_sec - start.tv_sec) * 1000 + (end_frame.tv_usec - start.tv_usec),
-           (end_compute.tv_sec - start.tv_sec) * 1000 + (end_compute.tv_usec - start.tv_usec));
+           cudaTime);
+
+    cudaEventDestroy(start_event);
+    cudaEventDestroy(stop_event);
 }
 
 int main(int argc, char **argv)
 {
     glutInit(&argc, argv);
-    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
     glutInitWindowSize(WIDTH, HEIGHT);
     glutInitWindowPosition(100, 100);
     glutCreateWindow("CUDA Demo");
@@ -89,14 +106,14 @@ int main(int argc, char **argv)
     glutIdleFunc(display);
 
     cudaMalloc((void**)&dev_bitmap, WIDTH * HEIGHT * sizeof(unsigned char));
-    cudaMalloc((void**)&dev_t, sizeof(int));
+    cudaMalloc((void**)&dev_time, sizeof(int));
     cudaMalloc((void**)&dev_waves, WAVES_COUNT * sizeof(Wave));
 
-    cudaMemcpy(dev_waves, waves, WAVES_COUNT * sizeof(Wave));
+    cudaMemcpy(dev_waves, waves, WAVES_COUNT * sizeof(Wave), cudaMemcpyHostToDevice);
 
     glutMainLoop();
 
     cudaFree(dev_bitmap);
-    cudaFree(dev_t);
+    cudaFree(dev_time);
     cudaFree(dev_waves);
 }
