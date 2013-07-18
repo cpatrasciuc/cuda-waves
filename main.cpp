2bc1a3986bf016a4d5b49b7db9a9db408f76e95a
Cristian Patrasciuc <calligrapha@gmail.com>
2013-07-18 14:41:54 +0300

    Add license information.

--- main.cpp
+++ main.cpp
@@ -1,3 +1,7 @@
+// Copyright (c) 2011 Cristian Patrasciuc. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 #include <time.h>
 #include <sys/time.h>
 #include <stdio.h>
